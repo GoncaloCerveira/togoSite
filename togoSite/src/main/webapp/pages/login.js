ef7cce3a7e52dc6d77e74902217e37a1e0cbf382
Martim Gamboa <m.gamboa@campus.fct.unl.pt>
2023-11-20 15:11:49 +0000

    firebase auth v2.0

--- togoSite/src/main/webapp/pages/login.js
+++ togoSite/src/main/webapp/pages/login.js
@@ -1,19 +1,56 @@
-firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-        // Signed in
-        var user = userCredential.user;
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-    });
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-firebase.auth().signInWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-        // Signed in
-        var user = userCredential.user;
-    })
-    .catch((error) => {
-        var errorCode = error.code;
-        var errorMessage = error.message;
-    });
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCnYbqrefRPaXQWR06l554KvNC7rjIEB9o",
+    authDomain: "startupipm.firebaseapp.com",
+    projectId: "startupipm",
+    storageBucket: "startupipm.appspot.com",
+    messagingSenderId: "995574355548",
+    appId: "1:995574355548:web:aa0fa875180b128de4b899",
+    measurementId: "G-NJYNRXD6B4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+function registerUser() {
+    const auth = getAuth();
+    const email = document.getElementById('emailR').value;
+    const password = document.getElementById('password').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up
+            const user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+        });
+}
+function loginUser() {
+    const auth = getAuth();
+    const email = document.getElementById('regEmail').value;
+    const password = document.getElementById('regPassword').value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+}
+
